d625f852ba828a9a34e199172904f629feef7813
THMvanderLaan <62060597+Fedor1997@users.noreply.github.com>
2025-12-31 09:45:53 +0100

    Add new function readrawsurface to readpostpro.py
    Enables to read out raw surface field data of probe

--- fluidfoam/readpostpro.py
+++ fluidfoam/readpostpro.py
@@ -234,6 +234,86 @@ def readprobes(path, probes_name="probes", time_name="0", name="U"):
         tab = np.array([])
     return probes_loc, time_vect, tab
 
+def readrawsurface(path: str, namepatch: str="forces", time_name: str="0", name: str="forces", region:str|None=None):
+    """read the raw data contained in the file about the surface
+    TODO rewrite
+    Args:
+        path: str\n
+        namepatch: str\n
+        time_name: str ('latestTime' and 'mergeTime' are supported)\n
+        name: str \n
+        region: str, optional
+
+    Returns:
+        array: array of surface field; size of the array is the size of the
+        field
+
+    A way you might use me is:\n
+        force = readforce(path='path_of_OpenFoam_case', namepatch='forces',
+                          time_name='0', name='forces', region='fluidZone')
+
+    """
+
+    if os.path.exists(path+namepatch):
+        path_namepatch = path+namepatch
+    else:
+        path_namepatch = os.path.join(path,'postProcessing',region,namepatch,'surface')
+    if time_name == "latestTime":
+        time_name = _find_latesttime(path_namepatch)
+    elif time_name == "mergeTime":
+        time_list = []
+        dir_list = os.listdir(path_namepatch)
+        for directory in dir_list:
+            try:
+                float(directory)
+                time_list.append(directory)
+            except:
+                pass
+        time_list.sort(key=float)
+        time_list = np.array(time_list)
+        for timename in time_list:
+            try:
+                tab = readforce(path, namepatch, timename, name+"_"+timename)
+            except:
+                try:
+                    tab = readforce(path, namepatch, timename, name)
+                except:
+                    print("An exception occurred")
+            if "tab_merge" in locals():
+                for jj in range(np.size(tab[:, 0])):
+                    if tab[jj, 0] > tab_merge[-1, 0]:
+                        break
+                    else:
+                        continue
+                if jj + 1 < np.size(tab[:, 0]):
+                    tab_merge = np.concatenate([tab_merge, tab[jj:, :]])
+            else:
+                tab_merge = tab
+        return tab_merge
+
+    with open(os.path.join(path_namepatch, time_name, name + ".raw"), "rb") as f:
+        content = f.read()
+    data = content.split(b"\n")
+    j = 0
+    header = True
+    for dummy, line in enumerate(data[:-1]):
+        if "#".encode() in line:
+            j += 1
+        elif "#".encode() not in line and header is True:
+            header = False
+            line = line.replace(b"(", b"")
+            line = line.replace(b")", b"")
+            line = line.split()
+            tab = np.zeros([len(data) - j - 1, len(line)], dtype=float)
+            j = 0
+            tab[j, :] = np.array(line, dtype=float)
+        else:
+            j += 1
+            line = line.replace(b"(", b"")
+            line = line.replace(b")", b"")
+            line = line.split()
+            tab[j, :] = np.array(line, dtype=float)
+    return tab
 
 if __name__ == "__main__":
 
